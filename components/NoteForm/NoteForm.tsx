db49046aa378d1641a4faacbf80bbf8de0fa19ca
Artem Bagno <bgn.rtm@gmail.com>
2026-03-31 00:58:27 +0300

    fix store in lib

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -5,7 +5,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { createNote } from '@/lib/api';
-import { useNoteDraftStore } from '../../store/noteCreateStore';
+import { useNoteDraftStore } from '../../lib/store/noteCreateStore';
 import css from './NoteForm.module.css';
 
 /*
